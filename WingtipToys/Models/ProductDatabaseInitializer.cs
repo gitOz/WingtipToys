2d1b947a7b697489297314ecab985304d73946eb
weezyxbeezy <louietorres93@gmail.com>
2015-04-15 22:44:21 -0700

    y

--- WingtipToys/Models/ProductDatabaseInitializer.cs
+++ WingtipToys/Models/ProductDatabaseInitializer.cs
@@ -56,6 +56,7 @@ namespace WingtipToys.Models
                     CategoryID = 5,
                     CategoryName = "Rockets"
                 },
+            
             };
 
             return categories;
