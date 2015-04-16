92fbca969dfaae1eb7dc36396727ca048a92dedb
weezyxbeezy <louietorres93@gmail.com>
2015-04-15 18:56:47 -0700

    I just added some of part 7

--- WingtipToys/Models/ProductContext.cs
+++ WingtipToys/Models/ProductContext.cs
@@ -1,21 +1,17 @@
 ﻿using System.Data.Entity;
+
 namespace WingtipToys.Models
 {
-    /// <summary>
-    /// Context Class
-    /// To start using the classes for data access, you must define a context class. 
-    /// As mentioned previously, the context class manages the entity classes 
-    /// (such as the Product class and the Category class) and provides data access 
-    /// to the database. 
-    /// </summary>
     public class ProductContext : DbContext
     {
         public ProductContext()
-            : base( "WingtipToys" )
+            : base("WingtipToys")
         {
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<CartItem> ShoppingCartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
